b80eb49441d83cd8d73145173f9ce3826a8f3a88
Lucas Lima <lucaserolima@gmail.com>
2023-10-31 13:55:41 -0400

    Initial commit

---
+++ src/main/java/br/com/ero/runnerregister/RunnerRegisterApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.runnerregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ero/runnerregister/RunnerRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.runnerregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
