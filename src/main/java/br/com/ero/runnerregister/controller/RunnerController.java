42830e745ebce1e5e5327fba94ad2785fe165255
Lucas Lima <lucaserolima@gmail.com>
2023-10-31 20:49:09 -0400

    Criados a  o findById no Service e no Controller.

--- src/main/java/br/com/ero/runnerregister/controller/RunnerController.java
+++ src/main/java/br/com/ero/runnerregister/controller/RunnerController.java
@@ -4,25 +4,33 @@ import br.com.ero.runnerregister.model.Runner;
 import br.com.ero.runnerregister.service.RunnerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.rmi.ServerException;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("crazy-race")
-public class RunnerController
-{
+public class RunnerController {
     final RunnerService runnerService;
 
-    public RunnerController(RunnerService runnerService){
+    public RunnerController(RunnerService runnerService) {
         this.runnerService = runnerService;
     }
 
     @PostMapping
-    public ResponseEntity<Runner> saveRunner(@RequestBody Runner runner){
-            return ResponseEntity.status(HttpStatus.CREATED).body(runnerService.save(runner));
-        }
+    public ResponseEntity<Runner> saveRunner(@RequestBody Runner runner) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(runnerService.save(runner));
     }
+
+    @GetMapping("runners/{id}")
+    public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID tb_runnerId) {
+        Optional<Runner> runnerOptional = Optional.ofNullable(runnerService.findById(tb_runnerId));
+            return ResponseEntity.status(HttpStatus.OK).body(runnerOptional.get());
+    }
+}
+
+
+
+
--- src/main/java/br/com/ero/runnerregister/service/RunnerService.java
+++ src/main/java/br/com/ero/runnerregister/service/RunnerService.java
@@ -1,23 +1,27 @@
 package br.com.ero.runnerregister.service;
 
+import br.com.ero.runnerregister.exception.EntityNotFoundException;
 import br.com.ero.runnerregister.model.Runner;
 import br.com.ero.runnerregister.repository.RunnerRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@RequiredArgsConstructor
 @Service
 public class RunnerService {
 
     final RunnerRepository runnerRepository;
-
-    public RunnerService(RunnerRepository runnerRepository){
-        this.runnerRepository = runnerRepository;
-    }
-
 
     @Transactional
     public Runner save(Runner runner) {
        return runnerRepository.save(runner);
     }
 
+    @Transactional
+    public Runner findById(UUID id) {
+        return  runnerRepository.findById(id).orElseThrow(() ->new EntityNotFoundException(String.format("Runner Not Found", id)));
+    }
 }
